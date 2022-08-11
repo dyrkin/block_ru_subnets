b8c96fb4d7381ea4e700310c49ca5a5e2e697ace
Eugene Zadyra <untrance@gmail.com>
2022-08-11 12:44:36 +0200

    scraper

---
+++ scraper.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	url2 "net/url"
+	"os"
+	"strings"
+)
+
+func ExampleScrape() {
+	docv4 := getDoc("https://www.countryipblocks.net/acl.php")
+	ipv4 := collectCIDRs(docv4)
+	docv6 := getDoc("https://www.countryipblocks.net/ipv6_acl.php")
+	ipv6 := collectCIDRs(docv6)
+	save(ipv4, "ipv4")
+	save(ipv6, "ipv6")
+}
+
+func collectCIDRs(doc *goquery.Document) string {
+	return doc.Find("#textareaAll").Text()
+}
+
+func getDoc(url string) *goquery.Document {
+	var body url2.Values = map[string][]string{
+		"countries[]": {"RU"},
+		"format1":     {"1"},
+		"get_acl":     {"Create ACL"},
+	}
+
+	req, _ := http.NewRequest("POST", url, strings.NewReader(body.Encode()))
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+}
+
+func save(cidrs string, fileName string) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err = fmt.Fprintln(f, cidrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	ExampleScrape()
+}
